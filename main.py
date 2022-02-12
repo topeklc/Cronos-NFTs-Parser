cf2d193f6545d3a315f19c6d7ba11ba7cb0f9f3b
unknown <topeklc@gmail.com>
2022-02-13 00:55:51 +0100

    Rebuild to collect data from most of cronos NFTs, add type hints and docstrings. Started to rebuild rarity calculator.

--- main.py
+++ main.py
@@ -3,13 +3,39 @@ import requests
 import html
 import csv
 from time import sleep
-# 0xf518f16f6611f0b7a51496cfcba2ed954ec81112
-# 0x7D5f8F9560103E1ad958A6Ca43d49F954055340a
-CONTRACT_ADDRESS = '0xf518f16f6611f0b7a51496cfcba2ed954ec81112'
-pages = 1112
+
+CONTRACT_ADDRESS = "0x89dBC8Bd9a6037Cbd6EC66C4bF4189c9747B1C56"
+PAGES = 5
 
 
-def get_data(page):
+def get_trait_lst(lst: list) -> list:
+    """
+    Shortens the list to one that only contains traits.
+    Args:
+        lst: List of all metadata.
+
+    Returns:
+        List that contains only traits.
+    """
+    pointer = 0
+    for i, item in enumerate(lst):
+        if item[0].startswith("attributes"):
+            pointer = i
+            break
+    trait_lst = lst[pointer:]
+    return trait_lst
+
+
+def get_data(page: int) -> dict:
+    """
+    Parse data for given contract address and page.
+    Args:
+        page: Page number passed as integer.
+
+    Returns:
+        Dictionary ready to save to a file.
+
+    """
     html_code = requests.get(
         f"https://cronos.crypto.org/explorer/token/{CONTRACT_ADDRESS}/instance/{page}/metadata"
     )
@@ -24,37 +50,31 @@ def get_data(page):
         .split(",")
     )
     ls = [x.lstrip().split(":") for x in res]
-    print(ls)
     dic = {}
-    correcter = len(ls) - 23
     for i, x in enumerate(ls):
         if i == 0:
-            dic["ID"] = int(x[-1].split('#')[-1].replace("#", "").replace(" ", ""))
+            dic["ID"] = int(x[-1].split("#")[-1].replace("#", "").replace(" ", ""))
         elif i == 1:
             dic[x[0]] = "ipfs:" + x[2]
-        elif i - correcter == 7:
-            dic[ls[i + 1][1].strip()] = x[2].strip()
-        elif i - correcter > 8:
-            try:
-                if i % 2 != 0:
-                    dic[ls[i - correcter + 1][1].replace("]", "").strip()] = ls[i - correcter][1].strip()
-            except IndexError as error:
-                print(error)
-                continue
+    trait_lst = get_trait_lst(ls)
+    for i, attr in enumerate(trait_lst):
+        if i == 0:
+            dic[trait_lst[i + 1][1].strip()] = attr[2].strip()
+        elif i % 2 == 0:
+            dic[trait_lst[i + 1][1].replace("]", "").strip()] = trait_lst[i][1].strip()
     return dic
 
 
-nfts_data = []
-
-
-def main(pages):
-    for page in range(pages):
+def main():
+    """Runs above functions and write result to a file."""
+    nfts_data = []
+    for page in range(1, PAGES):
         try:
             print(f"working on page {page}...")
             nfts_data.append(get_data(page))
         except Exception as e:
             print(e)
-            print("Something goes wrong...")
+            print("Something went wrong...")
         sleep(0.3)
     print(nfts_data)
     with open("NFTs_data.csv", mode="w") as data:
@@ -63,4 +83,5 @@ def main(pages):
         )
         writer.writerow(nfts_data)
 
-main(pages)
+
+main()
--- rarity_calc.py
+++ rarity_calc.py
@@ -1,69 +1,43 @@
 import csv
 import ast
 
-with open("NFTs_data.csv", mode="r") as wac:
-    reader = csv.reader(wac, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    my_lst = []
 
-    for x in reader:
-        print(x)
-        for y in x:
-            print(y)
-            my_lst.append(ast.literal_eval(y))
+def read_file():
+    with open("NFTs_data.csv", mode="r") as wac:
+        reader = csv.reader(wac, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        my_lst = []
+        for x in reader:
+            for y in x:
+                my_lst.append(ast.literal_eval(y))
+    return my_lst
 
+
+my_lst = read_file()
 counter_dict = {}
 dict_trait_points = {}
 banned = ["ID", "image"]
+
+
+for dic in my_lst:
+    for trait in dic:
+        if trait not in banned:
+            counter_dict[trait] = []
+
+print(counter_dict)
+
 for dct in my_lst:
     for x in dct:
         if x not in banned:
-            counter_dict[dct[x]] = 0
-
-for dct in my_lst:
-    for y in dct:
-        if y not in banned:
-            dict_trait_points[dct[y]] = 0
-
+            if {dct[x]: 0} not in counter_dict[x]:
+                counter_dict[x].append({dct[x]: 0})
 
 for dct in my_lst:
     for i in dct:
         if i not in banned:
-            counter_dict[dct[i]] += 1
+            for x in counter_dict[i]:
+                a = next(iter(x))
+                if a in list(dct.values()):
+                    x[a] += 1
+
 
 print(counter_dict)
-
-sorted_dct = {k: v for k, v in sorted(counter_dict.items(), key=lambda item: item[1])}
-
-print(sorted_dct)
-
-
-total = len(sorted_dct)
-
-
-def occurrences_to_points(occurrences):
-    points = total - occurrences
-    return points
-
-
-for k in sorted_dct:
-    dict_trait_points[k] = occurrences_to_points(sorted_dct[k])
-
-# print(dict_trait_points)
-traits = dict_trait_points.keys()
-
-id_points_dict = {}
-
-
-for item in my_lst:
-    id_points_dict[item["ID"]] = 0
-    ape_traits = [i for i in item.values()]
-    for trait in traits:
-        # print(trait)
-        if trait in ape_traits:
-            id_points_dict[item["ID"]] += dict_trait_points[trait]
-
-sorted_points_dict = {
-    k: v for k, v in sorted(id_points_dict.items(), key=lambda item: item[1])
-}
-
-print(sorted_points_dict)
